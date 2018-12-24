4db827a56b5d4135f87701391c4a8fff7be4c25a
ellango85 <ellan_bio3@yahoo.com>
2018-12-23 23:25:57 -0500

    Add files via upload

---
+++ Assignment-2-Inverse-matrix.R
@@ -0,0 +1,29 @@
+##Couple of functions that cache the inverse of a matrix
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solveMatrix) inv <<- solveMatrix
+  getInverse <- function() inv
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
+
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setInverse(inv)
+  inv      
+}
+
+# data <- matrix(c(1, 2, 3, 0, 2, 1, 3, 3, 0), nrow=3, ncol=3)
